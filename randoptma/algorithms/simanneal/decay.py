25924a5bde48a556006cf4a58390f4fe6173cb75
Mohamed Abouelsaadat <mohamed.abouelsaadat@gmail.com>
2024-03-06 19:49:11 +0100

    Return schedule label for decay schedules

--- randoptma/algorithms/simanneal/decay.py
+++ randoptma/algorithms/simanneal/decay.py
@@ -8,12 +8,14 @@ import math
 
 class Bezier:
     def __init__(self, init_T: float = 100, mid_point: float = 50) -> None:
-        self.span = init_T
         self.mid_pt = mid_point
         self.itr = -1
         self.init_T = init_T
         self.a = init_T - 2 * self.mid_pt
         self.b = 2 * self.mid_pt
+
+    def __str__(self) -> str:
+        return f"Bz {round(self.mid_pt/self.init_T, ndigits=2)}"
 
     def next_T(self) -> float:
         self.itr = max(0, min(self.itr + 1, self.init_T))
@@ -21,7 +23,7 @@ class Bezier:
             t = self.itr / self.b
         else:
             t = (-self.b + math.sqrt(self.b**2 + 4 * self.a * self.itr)) / (2 * self.a)
-        return (1 - t) ** 2 * self.span + 2 * (1 - t) * t * self.mid_pt
+        return (1 - t) ** 2 * self.init_T + 2 * (1 - t) * t * self.mid_pt
 
 
 class Logarithmic:
@@ -48,9 +50,15 @@ class TunedGeometric:
     def __init__(
         self, init_T: float = 1e3, a: float = 0.8, tuned_val: float = 100
     ) -> None:
+        self.init_T = init_T
+        self.tuned_val = tuned_val
+        self.a_originl = a
         self.factor = init_T / tuned_val
         self.a = a ** (1 / self.factor)
         self.T = tuned_val / self.a
+
+    def __str__(self) -> str:
+        return f"TG {self.a_originl}/{self.tuned_val}"
 
     def next_T(self):
         self.T = self.a * self.T
